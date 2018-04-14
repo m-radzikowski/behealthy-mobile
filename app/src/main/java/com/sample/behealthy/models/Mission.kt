6bc36ca3ab99c23bf29394233c4a550eaa52d8c1
Mateusz Cichosz <cmateuszm@gmail.com>
2018-04-14 15:27:39 +0200

    dodanie listy do misji oraz questów

--- app/src/main/java/com/sample/behealthy/models/Mission.kt
+++ app/src/main/java/com/sample/behealthy/models/Mission.kt
@@ -2,4 +2,4 @@ package com.sample.behealthy.models
 
 import java.util.*
 
-data class Mission(var title: String, var description: String, var expDate: Date, var quests: Array<Quest>)
+data class Mission(var title: String, var description: String, var expDate: Date, var quests: Array<Quest>, var done: Boolean)
---
+++ app/src/main/java/com/sample/behealthy/widget/MobileArrayAdapter.java
@@ -0,0 +1,43 @@
+package com.sample.behealthy.widget;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.sample.behealthy.R;
+import com.sample.behealthy.models.Quest;
+
+import java.util.List;
+
+public class MobileArrayAdapter extends ArrayAdapter<Quest> {
+	private final Context context;
+	private final List<Quest> values;
+
+	public MobileArrayAdapter(Context context, List<Quest> values) {
+		super(context, R.layout.list_item, values);
+		this.context = context;
+		this.values = values;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		LayoutInflater inflater = (LayoutInflater) context
+			.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+		View rowView = inflater.inflate(R.layout.list_item, parent, false);
+		TextView textView = (TextView) rowView.findViewById(R.id.firstLine);
+		TextView secondLine = (TextView) rowView.findViewById(R.id.secondLine);
+		ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+		for (Quest value: values) {
+			textView.setText(value.getTitle());
+			secondLine.setText(value.getDescription());
+		}
+
+
+		return rowView;
+	}
+}
