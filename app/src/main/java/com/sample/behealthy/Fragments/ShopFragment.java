d44c2539a35eb5de98a732fa0429c032e42c6114
Jakub <jjwisniewski92@gmail.com>
2018-04-14 13:35:01 +0200

    added initial shop fragment ui, added initial shop icons

--- app/src/main/java/com/sample/behealthy/Fragments/ShopFragment.java
+++ app/src/main/java/com/sample/behealthy/Fragments/ShopFragment.java
@@ -1,21 +1,64 @@
 package com.sample.behealthy.Fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.sample.behealthy.R;
 
 public class ShopFragment extends Fragment {
 
+	LinearLayout goldLL;
+	ImageView chestIV;
+
+	private boolean chestOpened = false;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	                         Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_section_launchpad, container, false);
+		View rootView = inflater.inflate(R.layout.fragment_shop, container, false);
 
+		goldLL = rootView.findViewById(R.id.gold_layout);
+		goldLL.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onGoldLayoutClick();
+			}
+		});
+
+		chestIV = rootView.findViewById(R.id.chest_icon);
+		chestIV.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onChestClick();
+			}
+		});
 
 		return rootView;
+	}
+
+	private void onGoldLayoutClick() {
+		AlertDialog alertDialog = new AlertDialog.Builder(getContext()).create();
+		alertDialog.setTitle("Shop Dialog");
+		alertDialog.setMessage("Later this will contain items to buy with gold");
+		alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+			new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.dismiss();
+				}
+			});
+		alertDialog.show();
+	}
+
+	private void onChestClick() {
+		chestOpened = !chestOpened;
+		chestIV.setImageDrawable(getResources().getDrawable(chestOpened ?
+			R.drawable.chest_open : R.drawable.chest_closed));
 	}
 }
